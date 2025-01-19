1b672237d5530aaa5d64ee66ae9c212ced11b7ca
Julien Delsescaux <jdlxt@locahost.dev>
2025-01-19 17:35:51 +0100

    sync

--- src/Controller/YoutubeController.php
+++ src/Controller/YoutubeController.php
@@ -6,6 +6,7 @@ use App\Entity\Media;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -150,6 +151,104 @@ class YoutubeController extends AbstractController
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
+    }
+
+
+    #[Route('/api/search', methods: ['GET'])]
+    public function search(Request $request): JsonResponse
+    {
+        $query = $request->query->get('q');
+        $query = trim($query);
+
+        if (!$query) {
+            return $this->json(['error' => 'Aucun mot-clé fourni'], 400);
+        }
+
+
+        // normalize query
+        $slug = $query;
+        $slug = mb_strtolower($slug);
+        $slug = preg_replace('/\s+/', '-', $slug);
+
+
+        $mediaRepository = $this->entityManager->getRepository(Media::class);
+        $existingSearch = $mediaRepository->findOneBy(['api_id' => $slug]);
+        if ($existingSearch) {
+            $response = $this->json([
+                'api_id' => $existingSearch->getApiId(),
+                'data' => $existingSearch->getData(),
+                'source' => 'database' // Indique que les données viennent de la BDD
+            ]);
+
+            $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+            return $response;
+        }
+
+
+
+        $url = "https://www.googleapis.com/youtube/v3/search";
+        $params = [
+            'query' => [
+                'part' => 'snippet',
+                'q' => $query,
+                'type' => 'video',
+                'maxResults' => 100,
+                'key' => $this->apiKey,
+            ]
+        ];
+
+        $response = $this->httpClient->request('GET', $url, $params);
+        if ($response->getStatusCode() !== 200) {
+            return $this->json(['error' => 'Erreur lors de la requête à l\'API YouTube'], 500);
+        }
+
+        $data = $response->toArray();
+
+        if(!$data) {
+            return $this->json(['error' => 'Aucun résultat trouvé'], 404);
+        }
+
+        $search = new Media();
+        $search->setType('youtube-search');
+        $search->setApiId($slug);
+        $search->setData($data);
+        $search->setSource('youtube-api');
+        $search->setType('youtube-search');
+
+        $this->entityManager->persist($search);
+        $this->entityManager->flush();
+
+        foreach ($data['items'] as $item) {
+            $videoId = $item['id']['videoId'];
+            $existingVideo = $mediaRepository->findOneBy(['api_id' => $videoId]);
+            if ($existingVideo) {
+                continue;
+            }
+
+            $video = new Media();
+            $video->setType('youtube-video');
+            $video->setApiId($videoId);
+            $video->setData($item);
+            $video->setSource('youtube-api');
+
+            $this->entityManager->persist($video);
+            $this->entityManager->flush();
+        }
+
+        $response = $this->json([
+            'api_id' => $search->getApiId(),
+            'data' => $data,
+            'source' => 'youtube-api'
+        ]);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        return $response;
+
     }
 
 
